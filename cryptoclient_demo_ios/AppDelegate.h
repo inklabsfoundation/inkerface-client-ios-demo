111ccb0a8328c2ccd433fbc990ea3603c2bade2d
wangh09 <wanghaoforever@126.com>
2018-05-30 11:23:09 +0800

    Initial Commit

---
+++ cryptoclient_demo_ios/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  cryptoclient_demo_ios
+//
+//  Created by 王昊 on 2018/5/30.
+//  Copyright © 2018年 inklabsfoundation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ cryptoclient_demo_ios/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  cryptoclient_demo_ios
+//
+//  Created by 王昊 on 2018/5/30.
+//  Copyright © 2018年 inklabsfoundation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
